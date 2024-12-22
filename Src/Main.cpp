e66fd1d16349e026014b712542bebca246c56b14
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-12-22 21:02:04 +0200

    Change plot example.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,5 +1,6 @@
 #include <Core.hpp>
 #include <OS/Window.hpp>
+#include <OS/Thread.hpp>
 #include <GUI/Basic.hpp>
 #include <Math/Algebra/Matrix.hpp>
 #include <Image/RawCPUImage.hpp>
@@ -13,23 +14,29 @@ int main()
     GUI::Init();
 
     RawCPUImage img(1024, 512, EFormat::RGBA8, true);
-    SmoothParametricPlot2D
+    Thread plottingThread
     (
-        img,
-        Vector<F64, 2>(0, 0.2),
-        Vector<F64, 2>(1, 0.8),
-        Vector<F64, 3>(0, -1, -1),
-        Vector<F64, 3>(6.28, 1, 1),
-        [](F64 t) -> Vector<F64, 2>
+        [&]() -> Void
         {
-            return Vector<F64, 2>
+            SmoothParametricPlot2D
             (
-                Sin(Sin(5 * t)) * Cos(0.01 * t) * Cos(10 * t),
-                Cos(Cos(7 * t)) * Sin(20 * t)
+                img,
+                Vector<F64, 2>(0, 0.2),
+                Vector<F64, 2>(1, 0.8),
+                Vector<F64, 3>(0, -1, -1),
+                Vector<F64, 3>(6.28, 1, 1),
+                [](F64 t) -> Vector<F64, 2>
+                {
+                    return Vector<F64, 2>
+                    (
+                        Sin(Sin(15 * t)) * Cos(0.01 * t) * Cos(10 * t) * Pow(Cos(20 * t), 2.0),
+                        Cos(Cos(17 * t)) * Sin(20 * t) * Pow(Sin(Sin(50 * t)), 4.0)
+                    );
+                },
+                Color4(0, 1, 1, 1),
+                1.0
             );
-        },
-        Color4(0, 1, 1, 1),
-        1.0
+        }
     );
     Window::AddToDrawAfterClear(img);
 
