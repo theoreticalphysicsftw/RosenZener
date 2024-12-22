341d9232eb8eb0cc9422f81015f3da09cb5e4e57
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-12-22 20:52:13 +0200

    Switch plot example.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -13,16 +13,23 @@ int main()
     GUI::Init();
 
     RawCPUImage img(1024, 512, EFormat::RGBA8, true);
-    SmoothPlot2D
+    SmoothParametricPlot2D
     (
         img,
-        Vector2(0, 0.3),
-        Vector2(1, 0.5),
-        Vector2(0, -1),
-        Vector2(1, 1),
-        [](F32 t) -> F32 { return Sin(200 * t) + Sin(500 * t + 200); },
+        Vector<F64, 2>(0, 0.2),
+        Vector<F64, 2>(1, 0.8),
+        Vector<F64, 3>(0, -1, -1),
+        Vector<F64, 3>(6.28, 1, 1),
+        [](F64 t) -> Vector<F64, 2>
+        {
+            return Vector<F64, 2>
+            (
+                Sin(Sin(5 * t)) * Cos(0.01 * t) * Cos(10 * t),
+                Cos(Cos(7 * t)) * Sin(20 * t)
+            );
+        },
         Color4(0, 1, 1, 1),
-        1.f
+        1.0
     );
     Window::AddToDrawAfterClear(img);
 
