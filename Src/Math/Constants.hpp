b7fd8230759fe6ec030dcb50fe98ae830b4f4b28
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2025-01-13 00:22:04 +0200

    Add some more constants.

--- Src/Math/Constants.hpp
+++ Src/Math/Constants.hpp
@@ -38,4 +38,5 @@ struct Constants
 
 	inline static constexpr Scalar Planck = 1.054571817e-34;
 	inline static constexpr Scalar Pi = 3.141592653589793238462643383279502884L;
+	inline static constexpr Scalar EulerMascheroni = 0.577215664901532860606512090082L;
 };
